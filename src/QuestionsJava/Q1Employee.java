c19c93d549e05d00e06e10a65f16910a81877ba8
Emanuel Júnior <junior.ssu57@gmail.com>
2023-05-08 19:42:56 -0300

    Question1
    Primeira questão completa!

---
+++ src/QuestionsJava/Q1Employee.java
@@ -0,0 +1,46 @@
+package QuestionsJava;
+
+public class Q1Employee {
+	private String name;
+	private float salary;
+	private float bonus;
+	private float liqSalary;
+	
+	public Q1Employee(String name, float salary) {
+		setName(name);
+		setSalary(salary);
+		calcLiqSalary();
+	}
+	public void calcLiqSalary() {
+		if(salary<=1000) {
+			bonus+=salary*0.20;
+			liqSalary=salary+bonus;
+		}
+		else if(salary>1000 && salary<=2000){
+			bonus+=salary*0.10;
+			liqSalary=salary+bonus;
+		}
+		else if(salary>2000){
+			bonus+=salary*0.10;
+			liqSalary=salary-bonus;
+		}
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public float getSalary() {
+		return salary;
+	}
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
+	public float getBonus() {
+		return bonus;
+	}
+	public float getLiqSalary() {
+		return liqSalary;
+	}
+}
