d85d052384c0ccc7309cf611b8c3b1165619442e
Emanuel Júnior <117325229+EmanuelJnr@users.noreply.github.com>
2023-05-11 15:32:04 -0300

    Aplicação do Strategy Pattern
    Foi modificado algumas classes e aplicado o Padrão de Projeto Strategy.

--- src/Question03/Question3.java
+++ src/Question03/Question3.java
@@ -6,8 +6,7 @@ import java.util.Scanner;
 public class Question3 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		ArrayList<String> questions;
-		ArrayList<String> answers;
+		QuestionsAnswers QA;
 		
 		System.out.print("Enter your name: ");
 		String name = input.nextLine();
@@ -15,14 +14,12 @@ public class Question3 {
 		System.out.println("1-General knowledge questions");
 		System.out.println("2-Anime questions");
 		String op = input.nextLine();
-		if(op.equals("1")) {
-			questions = QuestionsAnswers.generalQuestions();
-			answers = QuestionsAnswers.generalAnswers();
-		}
-		else{
-			questions = QuestionsAnswers.animeQuestions();
-			answers = QuestionsAnswers.animeAnswers();
-		}
+		if(op.equals("1"))
+			QA = new General();
+		else
+			QA = new Anime();
+		ArrayList<String> questions = QA.questions();
+		ArrayList<String> answers = QA.answers();
 		
 		int contRight=0;
 		for(int i=0;i<questions.size();i++) {
@@ -36,7 +33,8 @@ public class Question3 {
 			else
 				System.out.println("Wrong answer");
 		}
-		System.out.println("User "+name);
+		
+		System.out.println("\nUser "+name);
 		System.out.println("Right: "+contRight);
 		System.out.println("Wrong: "+(answers.size()-contRight));
 		
--- src/Question03/QuestionsAnswers.java
+++ src/Question03/QuestionsAnswers.java
@@ -2,61 +2,7 @@ package Question03;
 
 import java.util.ArrayList;
 
-public abstract class QuestionsAnswers {
-	public static ArrayList<String> generalQuestions(){
-		ArrayList<String> questions = new ArrayList<>();
-		questions.add("Que imperador pôs fogo em Roma?");
-		questions.add("Qual é o nome dado ao estado da água em forma de gelo?");
-		questions.add("Quando é comemorado o dia da independência do Brasil?");
-		questions.add("Quem é a namorada do Mickey?");
-		questions.add("Qual é o personagem do folclore brasileiro que tem uma perna só?");
-		questions.add("Quem foi o grande amor de Julieta?");
-		questions.add("Quem proclamou a república no Brasil em 1889?");
-		questions.add("Qual é a pedra preciosa mais dura encontrada na natureza?");
-		questions.add("Qual personagem da turma da Mônica tem apenas cinco fios de cabelo?");
-		questions.add("Quem fundou a Microsoft?");
-		return questions;
-	}
-	public static ArrayList<String> animeQuestions(){
-		ArrayList<String> questions = new ArrayList<>();
-		questions.add("Naruto Habita o espírito de qual Bijuu dentro de seu Corpo?");
-		questions.add("Qual o nome do personagem principal de Yu-Yu Hakusho?");
-		questions.add("Kempachi Zaraki é o capitão de que Divisão de guarda da corte no Anime?");
-		questions.add("Light após perceber que o Detetive L o investigava, tentou descobrir sua verdadeira identidade para...");
-		questions.add("Em Cavaleiros do Zodíaco, qual a armadura do Shiryu?");
-		questions.add("Ao pegar todas as Esferas do Dragão é concedido a pessoa que as encontrou...");
-		questions.add("Em Kakegurui, qual a aposta mais perigosa até agora?");
-		questions.add("No anime Super Onze, o personagem Endo possui que posição no seu time?");
-		questions.add("No anime Sword Art Online, Kirigaya e Asuna são no jogo e na vida social...");
-		questions.add("Em Demon Slayer, quais respirações Tanjiro pode usar?");
-		return questions;
-	}
-	public static ArrayList<String> generalAnswers(){
-		ArrayList<String> answers = new ArrayList<>();
-		answers.add("nero");
-		answers.add("estado sólido");
-		answers.add("7 de setembro ou sete de setembro");
-		answers.add("minnie mouse");
-		answers.add("saci pererê");
-		answers.add("romeu");
-		answers.add("marechal deodoro da fonseca");
-		answers.add("diamante");
-		answers.add("cebolinha");
-		answers.add("bill gates");
-		return answers;
-	}
-	public static ArrayList<String> animeAnswers(){
-		ArrayList<String> answers = new ArrayList<>();
-		answers.add("kyuubi");
-		answers.add("yusuke");
-		answers.add("divisão 11 ou divisão onze");
-		answers.add("matá-lo");
-		answers.add("armadura do dragão");
-		answers.add("um desejo ou 1 desejo");
-		answers.add("apostar a própria vida");
-		answers.add("goleiro");
-		answers.add("namorados");
-		answers.add("água e sol");
-		return answers;
-	}
+public interface QuestionsAnswers {
+	public ArrayList<String> questions();
+	public ArrayList<String> answers();
 }
