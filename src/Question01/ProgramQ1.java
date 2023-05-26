ee0ae86f64a35578b49e4f87726c93251d185399
Emanuel Júnior <117325229+EmanuelJnr@users.noreply.github.com>
2023-05-25 22:54:57 -0300

    Lista13 ok

--- src/Question01/ProgramQ1.java
+++ src/Question01/ProgramQ1.java
@@ -7,25 +7,40 @@ import java.util.Scanner;
 
 public class ProgramQ1 {
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
+		/*
+		 * pesquisar Java SE 15 ultilização das variáveis
+		 * usar var para não deixar anbíguo
+		 * 
+		 * usar uma classe apenas para validação, inputs e impressão
+		 * usar big decimal para valores monetários...
+		 * usar wrapers
+		 * pesquisar sobre o Logger, Kubernetes e @Slij4
+		 * estudar um pouco sobre NoSQL
+		 * idioma em inglês
+		 * separar responsabilidade
+		 * tratamento de exceção
+		 * utilizar o "var"
+		 * utilização dos tipos primitivos e classes wrapper
+		 */
+		var input = new Scanner(System.in);
 		ArrayList<Employee> employees = new ArrayList<>();
-		
+
 		System.out.print("Indicate how many employees there are in the company: ");
 		int amount = Integer.parseInt(input.nextLine());
-		
-		for(int i=1;i<=amount;i++) {
-			System.out.print("Enter the name of the "+i+"º employee: ");
+
+		for(int i=1;i<=amount;i++) {//new class only this code
+			System.out.print("Enter the name of the " + i + "º employee: ");
 			String name = input.nextLine();
 			System.out.print("Report the employee's salary: ");
 			float salary = Float.parseFloat(input.nextLine());
 			employees.add(new Employee(name, salary));
 		}
-		for(Employee employee : employees) {
-			NumberFormat dinheiro = NumberFormat.getCurrencyInstance(new Locale("pt","BR"));
-			System.out.println("\nEmployee: "+employee.getName());
-			System.out.println("Salary: R$ "+dinheiro.format(employee.getSalary()));
-			System.out.println("Bonus: R$ "+dinheiro.format(employee.getBonus()));
-			System.out.println("Liquid salary: R$ "+dinheiro.format(employee.getLiqSalary()));
+		for(Employee employee : employees) {//new class only this code
+			var dinheiro = NumberFormat.getCurrencyInstance(new Locale("pt","BR"));
+			System.out.println("\nEmployee: " + employee.getName());
+			System.out.println("Salary: " + dinheiro.format(employee.getSalary()));
+			System.out.println("Bonus: " + dinheiro.format(employee.getBonus()));
+			System.out.println("Liquid salary: " + dinheiro.format(employee.getLiqSalary()));
 		}
 		input.close();
 	}
